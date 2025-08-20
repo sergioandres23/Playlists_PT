f3bc643d9d6fbb3b58993f0792276c7c6dbca263
Sergio Gomez <sergio.gomezv@udea.edu.co>
2025-08-20 07:20:43 -0500

    chore: initialize project with pom.xml and basic structure

---
+++ src/test/java/com/sergio/playlists/playlistsms/PlaylistsMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sergio.playlists.playlistsms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlaylistsMsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
